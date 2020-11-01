f4c6f61e7f55f6f03133086935da2ec7713d70de
Luka Dimnik <luka.dimnik88@gmail.com>
2020-11-01 18:10:02 +0200

    exercises 4.6 & 4.7

--- part_4/blog-app/tests/list_helper.test.js
+++ part_4/blog-app/tests/list_helper.test.js
@@ -1,23 +1,30 @@
-const { favoriteBlog, dummy, totalLikes } = require("../utils/list_helper");
+const {
+  favoriteBlog,
+  dummy,
+  totalLikes,
+  mostBlogs,
+  mostLikes,
+} = require("../utils/list_helper");
 
-  test("dummy returns one", () => {
-    const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-    const result = dummy(blogs)
-    expect(result).toBe(1)    
-  });
+  const result = dummy(blogs);
+  expect(result).toBe(1);
+});
 
-describe('total likes', () => {
+describe("total likes", () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
-    }
-  ]
+      __v: 0,
+    },
+  ];
 
   const listOfBlogs = [
     {
@@ -47,32 +54,32 @@ describe('total likes', () => {
       url: "https://www.google.com/",
       likes: 15,
       id: "5ead5e7468b4d93c406b4182",
-    }
-  ]
+    },
+  ];
 
-  test('of empty list is zero', () => {
-    const result = totalLikes([])
-    expect(result).toBe(0)
-  })
+  test("of empty list is zero", () => {
+    const result = totalLikes([]);
+    expect(result).toBe(0);
+  });
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
-  })
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
 
-  test('of a bigger list is calculated right', () => {
-    const result = totalLikes(listOfBlogs)
-    expect(result).toBe(38)
-  })
-})
+  test("of a bigger list is calculated right", () => {
+    const result = totalLikes(listOfBlogs);
+    expect(result).toBe(38);
+  });
+});
 
-describe('favorite blog', () => {
+describe("favorite blog", () => {
   const listOfBlogs = [
     {
-      title: "Johnokjo",
+      title: "John",
       author: "Frank Sinatra",
       url: "https://www.google.com/",
-      likes: 5,
+      likes: 120,
       id: "5ead37cfbae5ea47a08f85a8",
     },
     {
@@ -86,7 +93,7 @@ describe('favorite blog', () => {
       title: "Hadoop",
       author: "Luka Dimnik",
       url: "https://www.google.com/",
-      likes: 10,
+      likes: 118,
       id: "5ead5970f131d944440d4194",
     },
     {
@@ -95,17 +102,49 @@ describe('favorite blog', () => {
       url: "https://www.google.com/",
       likes: 6,
       id: "5ead5e7468b4d93c406b4182",
-    }
-  ]
-
-  test('returns the right blog', () => {
-    const result = favoriteBlog(listOfBlogs)
-    expect(result).toEqual({
-      title: "Photoshop",
-      author: "Luka Dimnik",
+    },
+    {
+      title: "Dio mio",
+      author: "Dolores",
       url: "https://www.google.com/",
-      likes: 15,
-      id: "5ead571dbae5ea47a08f85a9",
-    })
-  })
-})
+      likes: 6,
+      id: "5ead5e7468b4d93c406b4182",
+    },
+    {
+      title: "Kekec",
+      author: "Dolores",
+      url: "https://www.google.com/",
+      likes: 8,
+      id: "5ead5e7468b4d93c406b4182",
+    },
+  ];
+
+  test("returns the right blog", () => {
+    const result = favoriteBlog(listOfBlogs);
+    expect(result).toEqual({
+      title: "John",
+      author: "Frank Sinatra",
+      url: "https://www.google.com/",
+      likes: 120,
+      id: "5ead37cfbae5ea47a08f85a8",
+    });
+  });
+
+  test("returns the author with most blogs", () => {
+    const result = mostBlogs(listOfBlogs);
+
+    expect(result).toEqual({
+      author: "Dolores",
+      blogs: 3,
+    });
+  });
+
+  test("returns the author with most likes", () => {
+    const result = mostLikes(listOfBlogs);
+
+    expect(result).toEqual({
+      author: "Luka Dimnik",
+      blogs: 133,
+    });
+  });
+});
--- part_4/blog-app/utils/list_helper.js
+++ part_4/blog-app/utils/list_helper.js
@@ -3,28 +3,79 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  let sum = 0
-  blogs.map((blog) => sum += blog.likes)
+  let sum = 0;
+  blogs.map((blog) => (sum += blog.likes));
   //console.log(sum)
-  return sum
-}
+  return sum;
+};
 
 const favoriteBlog = (blogs) => {
-  let mostLikesPosition = 0
+  let mostLikesPosition = 0;
   blogs.map((blog, i) => {
     if (blog.likes > blogs[mostLikesPosition].likes) {
-      mostLikesPosition = i
+      mostLikesPosition = i;
     }
-  })
-  return blogs[mostLikesPosition]
-}
+  });
+  return blogs[mostLikesPosition];
+};
 
 const mostBlogs = (blogs) => {
-    const authors = blogs.map((blog) => blog.author)
-}
+  const numBlogsByAuthor = {};
+  const authors = blogs.map((blog) => blog.author);
+  for (a of authors) {
+    if (!numBlogsByAuthor[a]) {
+      numBlogsByAuthor[a] = 1;
+    } else {
+      numBlogsByAuthor[a] += 1;
+    }
+  }
+  let authorWithMostBlogs = "";
+  let max = 0;
+  for (a of authors) {
+    if (numBlogsByAuthor[a] > max) {
+      authorWithMostBlogs = a;
+      max = numBlogsByAuthor[a];
+    }
+  }
+  return {
+    author: authorWithMostBlogs,
+    blogs: numBlogsByAuthor[authorWithMostBlogs],
+  };
+};
+
+const mostLikes = (blogs) => {
+  const numLikesByAuthor = {};
+  const authors = blogs.map((blog) => blog.author);
+  for (b of blogs) {
+    if (!numLikesByAuthor[b.author]) {
+      numLikesByAuthor[b.author] = b.likes;
+    } else {
+      numLikesByAuthor[b.author] += b.likes;
+    }
+  }
+
+  console.log("numLikesByAuthor", numLikesByAuthor);
+  let authorWithMostBlogs = "";
+  let max = 0;
+  for (a of Object.keys(numLikesByAuthor)) {
+    if (numLikesByAuthor[a] > max) {
+      authorWithMostBlogs = a;
+      max = numLikesByAuthor[a];
+    }
+  }
+
+  const returnObject = {
+    author: authorWithMostBlogs,
+    blogs: numLikesByAuthor[authorWithMostBlogs],
+  };
+  console.log("returnObject", returnObject);
+  return returnObject;
+};
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
